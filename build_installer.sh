8d224d5b1591abc6dbad595fdbbb45395afbd748
Daniel Golle <daniel@makrotopia.org>
2021-10-17 22:25:47 +0100

    rework OpenWrt package selection
     * procd-ujail is now installed by default, no need to list here again
     * add kmod-fs-exfat to support larger factory-formatted USB storage

--- build_installer.sh
+++ build_installer.sh
@@ -238,10 +238,10 @@ linksys_e8450_installer() {
 
 	OPENWRT_UPG_PACKAGES="auc blockd kmod-usb-storage kmod-usb-storage-uas kmod-fs-vfat \
 				kmod-nls-base kmod-nls-cp437 kmod-nls-iso8859-1 kmod-nls-utf8 \
-				kmod-fs-ext4 kmod-fs-f2fs procd-ujail luci luci-ssl-openssl \
-				luci-theme-openwrt-2020 -libustream-wolfssl -wpad-basic-wolfssl \
-				-px5g-wolfssl libustream-openssl wpad-openssl \
-				luci-app-attendedsysupgrade $OPENWRT_ADD_PACKAGES"
+				kmod-fs-ext4 kmod-fs-f2fs kmod-fs-exfat \
+				luci luci-ssl-openssl luci-theme-openwrt-2020 luci-app-attendedsysupgrade \
+				-libustream-wolfssl -wpad-basic-wolfssl -px5g-wolfssl libustream-openssl wpad-openssl \
+				$OPENWRT_ADD_PACKAGES"
 
 	run_openwrt_ib
 	BINDIR="${OPENWRT_DIR}/bin/targets/mediatek/mt7622"
